0a92eaa34c51e313c232b7314f3bfd13f0646b02
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2023-02-19 18:10:45 -0500

    Add temp slides data

---
+++ src/api/index.ts
@@ -0,0 +1,43 @@
+// TODO: replace with API call
+export const tempSlidesData = [
+  {
+    id: '1',
+    title: 'Slide 1',
+  },
+  {
+    id: '2',
+    title: 'Slide 2',
+  },
+  {
+    id: '3',
+    title: 'Slide 3',
+  },
+  {
+    id: '4',
+    title: 'Slide 4',
+  },
+  {
+    id: '5',
+    title: 'Slide 5',
+  },
+  {
+    id: '6',
+    title: 'Slide 6',
+  },
+  {
+    id: '7',
+    title: 'Slide 7',
+  },
+  {
+    id: '8',
+    title: 'Slide 8',
+  },
+  {
+    id: '9',
+    title: 'Slide 9',
+  },
+  {
+    id: '10',
+    title: 'Slide 10',
+  },
+];
